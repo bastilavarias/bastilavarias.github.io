4d468d0209f2d2e517a9f4b21ba83c851686b856
Sebastian <denzilbao@gmail.com>
2021-01-28 17:20:27 +0800

    first commit

---
+++ web-client/src/views/BlogItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="pt-10">
+    <v-row>
+      <v-col cols="12">
+        <span
+          class="overline d-flex align-center"
+          @click="$router.go(-1)"
+          :style="{ cursor: 'pointer' }"
+        >
+          <v-icon small class="mr-1">mdi-arrow-left</v-icon>
+          <span>Go Back</span>
+        </span>
+      </v-col>
+      <v-col cols="12">
+        <v-card flat color="transparent">
+          <v-card-title>Blog Title</v-card-title>
+          <v-card-subtitle>January 28, 2021</v-card-subtitle>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+            accusantium aperiam architecto delectus eaque facilis fuga fugit, id
+            incidunt minus molestiae nisi rem rerum, suscipit vero? A accusamus
+            accusantium consequatur deserunt dolor, ea eligendi ex explicabo hic
+            in labore laudantium libero pariatur perferendis perspiciatis
+            placeat possimus quasi quia ratione similique sint soluta suscipit
+            tempore ullam unde voluptate voluptatem. Aliquid, aperiam cumque
+            fuga fugit in laborum magnam maiores minima nemo porro quia
+            recusandae tempora, tenetur? Aliquid, assumenda dolor doloribus esse
+            eveniet fugit, ipsam maxime modi obcaecati optio porro possimus qui
+            quibusdam rerum sint vitae, voluptates. Explicabo impedit nesciunt
+            optio quasi quo.
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </section>
+</template>
---
+++ web-client/src/views/Blogs.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="pt-10">
+    <v-row>
+      <v-col cols="12">
+        <span class="overline">Blogs</span>
+      </v-col>
+      <v-col cols="12">
+        <template v-for="(blog, index) in blogs">
+          <v-list-item :key="index" three-line>
+            <v-list-item-content>
+              <v-list-item-title
+                ><router-link :to="blog.to" :style="{ textDecoration: 'none' }">
+                  <span class="font-weight-bold text--primary">{{
+                    blog.title
+                  }}</span>
+                </router-link></v-list-item-title
+              >
+              <v-list-item-subtitle>{{ blog.excerpt }}</v-list-item-subtitle>
+              <v-list-item-subtitle
+                >Created {{ blog.date }}</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-col>
+    </v-row>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [
+        {
+          title: "Blog 1",
+          excerpt: "Excerpt 1",
+          date: "January 28, 2021",
+          to: { name: "Blog Item Page", params: { slug: "blog-1" } }
+        },
+
+        {
+          title: "Blog 2",
+          excerpt: "Excerpt 2",
+          date: "January 28, 2021",
+          to: { name: "Blog Item Page", params: { slug: "blog-2" } }
+        },
+
+        {
+          title: "Blog 3",
+          excerpt: "Excerpt 3",
+          date: "January 28, 2021",
+          to: { name: "Blog Item Page", params: { slug: "blog-3" } }
+        }
+      ]
+    };
+  }
+};
+</script>
